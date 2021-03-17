d0e993a4de3f06703b8e8f4880a18d49544d94d9
Sam Jones <sam@smjdesign.co.uk>
2021-03-17 17:07:54 +0000

    Adding photo method and config

--- app/app.py
+++ app/app.py
@@ -1,1 +1,29 @@
-print("Hello world")
+import time, datetime, os, uuid, json
+from python_json_config import ConfigBuilder
+
+def init():
+    x = datetime.datetime.now()
+    print(x)
+
+    builder = ConfigBuilder()
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    config = builder.parse_config(dir_path + '/' + "config.json")
+    outputDirectory = config.outputDirectory
+
+def generateDate():
+    timestr = time.strftime("%Y%m%d%H%M%S")
+    return timestr
+
+def takePhoto():
+    image_filename = outputDirectory + "image%s.jpg"%(generateDate())
+    print("Saving photo as " +image_filename)
+    command = "raspistill -vf -o {0} -n --exposure auto".format(image_filename)
+    os.system(command)
+
+try:
+    init()
+    takePhoto()
+
+except Exception as ex:
+    print('Exception:')
+    print(ex)
